ca95b780bd04022de11665e36d45b18daf267488
hob71 <roberthobday@sky.com>
2020-11-08 10:57:55 +0000

    Removed secret key

--- djno_todo/settings.py
+++ djno_todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'ixlv6yl7pk$d06@c*ons5*ei$0fd9x)z9lw8meyz7$fnresj6')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
